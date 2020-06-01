d96b33d52a5ffa9807d86506a148887cdc85b015
Mihail Popovici <Mihail.Popovic@cedacrinternational.md>
2020-06-01 15:05:58 +0300

    feat: Add connection to bank interface and access it

--- app.rb
+++ app.rb
@@ -1,1 +1,8 @@
-puts "Hi, World! This one will we super mega app"
+require 'watir'
+
+browser = Watir::Browser.new
+
+browser.goto('https://demo.bendigobank.com.au/banking/sign_in')
+browser.button(name: 'customer_type').click
+
+browser.close

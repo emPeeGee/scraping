7a79cab3f7f5879ccc065331d88992f7b1b11444
Mihail Popovici <Mihail.Popovic@cedacrinternational.md>
2020-06-02 15:22:48 +0300

    fix: Amount symbol
    Amount symbol at client is just a CSS shape, so it comes without symbol, now we put symbol depending on color of amount

--- lib/parser.rb
+++ lib/parser.rb
@@ -55,28 +55,42 @@ class Parser < BrowserContainer
       puts @browser.driver.execute_script(end_of_page)
     end
 
+
     group_by_date_transactions = @browser.elements(css: 'li[data-semantic="activity-group"]')
     group_by_date_transactions.each do |el|
       transaction_date = Date.parse(el.element(class: "grouped-list__group__heading").text)
       #puts transaction_date
 
-      #
       list_of_transactions_group = el.elements(css: 'li[data-semantic="activity-item"]')
       list_of_transactions_group.each do |transaction_item|
+
         # Where is two '<span>' we need first, which is description
         transaction_description = transaction_item.elements(class: 'overflow-ellipsis')[0].text.strip
 
-        amount_with_currency = transaction_item.element(css: 'span[data-semantic="amount"]').text.strip
-        transaction_amount = amount_with_currency
+        span_amount_with_currency = transaction_item.element(css: 'span[data-semantic="amount"]')
+        amount_color = span_amount_with_currency.style("color")
+        amount_with_currency = span_amount_with_currency.text
+
+        transaction_amount = get_symbol_from_color(amount_color, amount_with_currency)
         transaction_currency = WORLD_CURRENCY[Utils.get_currency_symbol amount_with_currency]
+
         puts "#{transaction_date} #{transaction_description} #{transaction_amount} #{transaction_currency} #{account_name}"
       end
-      #
     end
 
     transactions
   end
 
+  def get_symbol_from_color(amount_color, amount_with_currency)
+    case amount_color
+    when "rgba(43, 43, 43, 1)"
+      "-" + Utils.balance_without_symbol(amount_with_currency)
+    when "rgba(30, 130, 76, 1)"
+      Utils.balance_without_symbol(amount_with_currency)
+    else
+      ''
+    end
+  end
 
   def wait
     sleep 3
